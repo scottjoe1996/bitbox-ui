d66cf0684e42e7dd8218bf733776e9b021ae256f
Joseph Scott <scottjoe1996@yahoo.co.uk>
2024-08-07 15:45:57 +0100

    fixed tests

--- src/components/button/button.spec.ts
+++ src/components/button/button.spec.ts
@@ -8,20 +8,20 @@ describe('button', () => {
       html: '<bit-button></bit-button>',
     });
     expect(root).toEqualHtml(`
-      <bit-button
+      <bit-button>
         <mock:shadow-root>
           <div>
             Hello, World! I'm
           </div>
         </mock:shadow-root>
-      </bit-button
+      </bit-button>
     `);
   });
 
   it('renders with values', async () => {
     const { root } = await newSpecPage({
       components: [Button],
-      html: `<bit-button first="Stencil" last="'Don't call me a framework' JS"></bit-button`,
+      html: `<bit-button first="Stencil" last="'Don't call me a framework' JS"></bit-button>`,
     });
     expect(root).toEqualHtml(`
       <bit-button first="Stencil" last="'Don't call me a framework' JS">
@@ -30,7 +30,7 @@ describe('button', () => {
             Hello, World! I'm Stencil 'Don't call me a framework' JS
           </div>
         </mock:shadow-root>
-      </bit-button
+      </bit-button>
     `);
   });
 });
