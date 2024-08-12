5f2164aced0257998848dd3805f19ae85211031d
Joseph Scott <scottjoe1996@yahoo.co.uk>
2024-08-12 22:14:01 +0100

    added full width test for button

--- src/components/button/button.e2e.ts
+++ src/components/button/button.e2e.ts
@@ -17,6 +17,17 @@ describe('bit-button', () => {
     expect(button.textContent).toEqual('Hello There!');
   });
 
+  it('renders with button width matching container when fullWidth is true', async () => {
+    const containerWidth = '500px';
+    const page = await newE2EPage();
+
+    await page.setContent(`<div style="width: ${containerWidth}"><bit-button full-width="true">Hello There!</bit-button><bit-button>Hello There!</bit-button><div>`);
+    const fullWidthButton = await page.find('bit-button[full-width="true"]');
+    const regularButton = await page.find('bit-button:not([full-width])');
+    expect((await fullWidthButton.getComputedStyle()).width).toEqual(containerWidth);
+    expect((await regularButton.getComputedStyle()).width).not.toEqual(containerWidth);
+  });
+
   it('should emit click event when clicked', async () => {
     const page = await newE2EPage();
 
