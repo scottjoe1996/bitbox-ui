49d428bddd6b9267acc191a0a326b66e9369463a
Joseph Scott <scottjoe1996@yahoo.co.uk>
2024-09-14 23:57:01 +0100

    created demo react project to test bitbox react components

---
+++ demos/react/src/app.tsx
@@ -0,0 +1,12 @@
+import { BitButton } from '@bitbox-ui/react';
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <BitButton variant="outlined">Button</BitButton>
+    </div>
+  );
+};
+
+export default App;
